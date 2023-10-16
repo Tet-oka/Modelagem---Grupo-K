1421969f8bfa96c65ea01dd4593d25980a8865e8
LucasRetoreCarboni <lucasretore.carboni@gmail.com>
2023-10-16 16:18:08 -0300

    organização final

--- Exercício 1/ex01_esp.m
+++
@@ -1,5 +0,0 @@
-    x(3);
-    x(4);
-    (-3*(\[Lambda]*((5 + 4*\[Lambda])*sin(x(1)) + 3*sin(x(1) - 2*x(2)))*(Subscript(\[Omega],p)*Subscript(\[Omega],p)) + 2*sin(x(1) - x(2))*(3*\[Lambda]*cos(x(1) - x(2))*(x(3)*x(3)) + 2*(x(4)*x(4)))))/(\[Lambda]*(15 + 8*\[Lambda] - 9*cos(2*(x(1) - x(2)))));
-    (3*(\[Lambda]*(3*(2 + \[Lambda])*sin(2*x(1) - x(2)) - (6 + \[Lambda])*sin(x(2)))*(Subscript(\[Omega],p)*Subscript(\[Omega],p)) + 2*sin(x(1) - x(2))*(2*\[Lambda]*(3 + \[Lambda])*(x(3)*x(3)) + 3*cos(x(1) - x(2))*(x(4)*x(4)))))/(15 + 8*\[Lambda] - 9*cos(2*(x(1) - x(2))));
-
