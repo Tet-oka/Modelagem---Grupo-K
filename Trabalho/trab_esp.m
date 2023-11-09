790db8cc0ea8e98523ace1daf9b5e5a4f8e8e417
LucasRetoreCarboni <lucasretore.carboni@gmail.com>
2023-11-09 10:53:38 -0300

    trabalho

---
+++ Trabalho/trab_esp.m
@@ -0,0 +1,7 @@
+    x(4);
+    x(5);
+    x(6);
+    -(((Subscript(c,r) + Subscript(c,t))*x(4))/m) + (Subscript(c,t)*x(5))/m + (-(g*m) + Subscript(k,t)*(-x(1) + x(2)) + Subscript(k,r)*(-x(1) + Subscript(y,ext)(t)) + Subscript(c,r)*Derivative(1)(Subscript(y,ext))(t))/m;
+    -((g*M - (S*\[Rho]*Subscript(C,L)*((Subscript(u,longit) + Subscript(u,vento))*(Subscript(u,longit) + Subscript(u,vento))))/2. + Subscript(k,t)*(-x(1) + x(2)))/M) + (Subscript(c,t)*x(4))/M - (Subscript(c,t)*x(5))/M;
+    (S*\[Rho]*sin(Subscript(\[CapitalPhi],0) + x(3))*Subscript(C,D)*Subscript(D,PO)*((Subscript(u,longit) + Subscript(u,vento))*(Subscript(u,longit) + Subscript(u,vento))) - S*\[Rho]*Subscript(C,L)*((Subscript(u,longit) + Subscript(u,vento))*(Subscript(u,longit) + Subscript(u,vento)))*(cos(Subscript(\[CapitalPhi],0) + x(3))*(Subscript(D,GO) - Subscript(D,PO)) + sin(Subscript(\[CapitalPhi],0) + x(3))*Subscript(D,GO)*Subscript(\[Mu],rol)) + 2*Subscript(D,GO)*(g*M*sin(Subscript(\[CapitalPhi],0) + x(3))*Subscript(\[Mu],rol) + cos(Subscript(\[CapitalPhi],0) + x(3))*Subscript(k,t)*(-x(1) + x(2))))/(2.*Subscript(J,oz)) - (cos(Subscript(\[CapitalPhi],0) + x(3))*Subscript(c,t)*Subscript(D,GO)*x(4))/Subscript(J,oz) + (cos(Subscript(\[CapitalPhi],0) + x(3))*Subscript(c,t)*Subscript(D,GO)*x(5))/Subscript(J,oz);
+
